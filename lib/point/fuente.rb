d87f61f1c6395581baa8c2490f113d3a5c82f66c
Miguel Parra <alu0100200393@ull.edu.es>
2017-11-22 11:44:15 +0000

    cambio prueba con travis

--- lib/point/fuente.rb
+++ lib/point/fuente.rb
@@ -2,31 +2,31 @@
 # Este módulo se ha creado para describir
 # distintas metodologías de programación
 # haciendo uso del Lenguaje de Programación
-# Ruby.  
+# Ruby.
 # Con ella se han desarrollado los ejemplos
-# de la asignatura Lenguajes y Paradigmas 
+# de la asignatura Lenguajes y Paradigmas
 # de Programación.
 #
 # Author::    Coromoto Leon  (mailto:cleon@x.y)
 # Copyright:: Cretive Commons
 # License::   Distributes under the same terms as Ruby
-
+# prueba con travis
 module Point
 
-  # Esta clase permite representar un punto en el espacio 
+  # Esta clase permite representar un punto en el espacio
   # bidimensional.
   # Se han incluido los mixin Comparable y Enumerable.
   class Point
     include Comparable
     include Enumerable
     attr_reader :x, :y
-    
-    # Se asignan las coordenadas x e y 
+
+    # Se asignan las coordenadas x e y
     def initialize(x,y)
       # los comentarios locales a los métodos no generan documentación
       @x, @y = x, y
     end
-    
+
 =begin
       Comprobando si esto es un comentario que
       incluye varias líneas
@@ -37,7 +37,7 @@ module Point
       # los comentarios locales a los métodos no generan documentación
       Point.new(@x * value, @y * value)
     end
-    
+
     # Opuesto a un punto
     # Este es un ejemplo de comentario en el que
     # se incluye esta parte.
@@ -50,23 +50,23 @@ module Point
       # los comentarios locales a los métodos no generan documentación
       Point.new(-@x, -@y)
     end
-    
+
     # Suma dos puntos
     def +(other)
       # los comentarios locales a los métodos no generan documentación
       Point.new(@x + other.x, @y + other.y)
     end
-    
+
     # Se define para incluir el mixin comparable
-    # Se toma como valor para la comparación al 
+    # Se toma como valor para la comparación al
     # distancia al origen.
     def <=>(other)
       # los comentarios locales a los métodos no generan documentación
       return nil unless other.instance_of? Point
       @x**2 + @y**2 <=> other.x**2 + other.y**2
     end
-    
-    # Tipeado pato 
+
+    # Tipeado pato
     # def ==(other)
     #  if ((other.respond_to?x) && (other.respond_to?y)) then
     #    @x == other.x && @y == other.y
@@ -95,7 +95,7 @@ module Point
         false
       end
     end
-    
+
     # Se incluye el metodo del mixin Enumerable
     # Se define como una iteración sobre las dos coordenadas
     def each #:nodoc:
